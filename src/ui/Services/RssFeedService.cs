4372e10e2403e98b9ad709ef391ae2b3b8e20fb7
Yann Duran <yd@live.com.au>
2019-05-03 11:02:07 +1000

    Add RSS feed service

---
+++ src/ui/Services/RssFeedService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace DeveloperNews.UI.Services
+{
+    using Core.Interfaces;
+    using Core.Models;
+
+    public class RssFeedService : IRssFeedService
+    {
+        public async Task<List<FeedItem>> GetFeedItemsAsync(string url)
+        {
+            var client = new HttpClient();
+            var result = await client.GetStringAsync(url);
+            var xdoc = XDocument.Parse(result);
+
+            return
+            (
+                from item in xdoc.Descendants("item")
+                select new FeedItem
+                {
+                    Title = (string)item.Element("title"),
+                    Description = (string)item.Element("description"),
+                    Link = (string)item.Element("link"),
+                    PublishDate = DateTime.Parse((string)item.Element("pubDate"))
+                }
+            ).ToList();
+        }
+    }
+}
