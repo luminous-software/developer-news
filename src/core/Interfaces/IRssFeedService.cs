38dae2ec82af0f8558765c6ff63abfbeb3aea79a
Yann Duran <yd@live.com.au>
2019-05-03 10:50:54 +1000

    Add feed interface & feed item model

---
+++ src/core/Interfaces/IRssFeedService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DeveloperNews.Core.Interfaces
+{
+    using Models;
+
+    public interface IRssFeedService
+    {
+        Task<List<FeedItem>> GetFeedItemsAsync(string url);
+    }
+}
