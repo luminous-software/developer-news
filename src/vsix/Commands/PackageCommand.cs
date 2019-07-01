bbfb1928a4493c09af2e12181c7923fa3d447607
Yann Duran <yd@live.com.au>
2019-07-01 16:56:10 +1000

    Add PackageCommand

---
+++ src/vsix/Commands/PackageCommand.cs
@@ -0,0 +1,15 @@
+﻿namespace DeveloperNews.Commands
+{
+    using Luminous.Code.VisualStudio.Commands;
+    using Luminous.Code.VisualStudio.Packages;
+
+    internal abstract class PackageCommand : AsyncDynamicCommand
+    {
+        protected PackageCommand(AsyncPackageBase package, int id) : base(package, id)
+        {
+        }
+
+        protected override bool CanExecute
+           => PackageClass.GeneralOptions.EnableDeveloperNews;
+    }
+}
--- src/vsix/Commands/ViewDeveloperNews.cs
+++ src/vsix/Commands/ViewDeveloperNews.cs
@@ -7,17 +7,17 @@ namespace DeveloperNews.Commands
 {
     using UI.Views.DevNews;
 
-    internal sealed class ViewDeveloperNews : AsyncDynamicCommand
+    internal sealed class ViewDeveloperNews : PackageCommand
     {
         private ViewDeveloperNews(AsyncPackageBase package)
-            : base(package, PackageIds.DeveloperNewsCommand)
+            : base(package, PackageIds.ViewDeveloperNews)
         { }
 
         public static async Tasks.Task InstantiateAsync(AsyncPackageBase package)
             => await InstantiateAsync(new ViewDeveloperNews(package));
 
         protected override bool CanExecute
-          => PackageClass.GeneralOptions.DeveloperNewsEnabled;
+          => PackageClass.GeneralOptions.EnableDeveloperNews;
 
         protected override void OnExecute(OleMenuCommand command)
             => ExecuteCommand()
