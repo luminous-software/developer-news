489da2b4e6854bab4eff9c937e5aeca72c76f949
Yann Duran <yd@live.com.au>
2020-04-10 10:40:23 +1000

    Bump version number to v1.1.0.3
    - extension not working at this point

--- src/vsix/source.extension.cs
+++ src/vsix/source.extension.cs
@@ -11,7 +11,7 @@ namespace DeveloperNews
         public const string Name = "Developer News";
         public const string Description = @"Bring Back Developer News!";
         public const string Language = "en-US";
-        public const string Version = "1.1.0.2";
+        public const string Version = "1.1.0.3";
         public const string Author = "Yann Duran";
         public const string Tags = "developer news, developer-news, developernews, start page, start-page, startpage, luminous, yannduran, yd";
     }
