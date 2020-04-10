c377317e7b989269804449b5f52cf929caddc1c9
Yann Duran <yd@live.com.au>
2020-04-10 18:31:05 +1000

    Rename NewsControl to NewsItemsView

---
+++ src/ui/Views/NewsItemsView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Controls;
+
+namespace DeveloperNews.UI.Views
+{
+    using ViewModels;
+
+    public partial class NewsItemsView : UserControl
+    {
+        public NewsItemsView()
+        {
+            var viewModel = ViewModelLocator.NewsItemsViewModel;
+
+            //viewModel.ExecuteRefresh();
+
+            InitializeComponent();
+
+            DataContext = viewModel;
+
+            NewsItemsListView.SelectionChanged += (sender, e) =>
+            {
+                var listView = (ListView)sender;
+
+                listView.SelectedItem = null;
+            };
+        }
+    }
+}
